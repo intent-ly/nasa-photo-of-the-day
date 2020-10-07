b09d00a7b0c8c703677ccfcb3de514eb7db06d49
intent-ly <erick.skillsusa@gmail.com>
2020-10-07 15:37:23 -0700

    Pull Data API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
+import { DEMO_KEY, SPACE_URL } from "./index";
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <p>
+//         Read through the instructions in the README.md file to build your NASA
+//         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+//       </p>
+//     </div>
+//   );
+// }
+
+export default function App() {
+  const [space , setSpace] = useState([])
+  const [currentSpaceId, setCurrentSpaceId] = useState(null)
+
+  const openDetails = id =>{
+    setCurrentSpaceId(id)
+  }
+
+  const closeDetails = () =>{
+    setCurrentSpaceId(null)
+  }
+  
+
+  //Fetch Data
+  useEffect(() =>{
+    const fetchSpace = () =>{
+      axios.get(`${SPACE_URL}apod?api_key=${DEMO_KEY}`)
+      .then (res =>{
+        setSpace(res.data)
+        console.log(res.data)
+      })
+      .catch(err =>{
+        debugger
+      })
+    }
+    fetchSpace()
+
+    return ()=>{
+      //Clear
+    }
+
+  }, [])
+
+
+  return (null)
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import "./index.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+export const SPACE_URL = 'https://api.nasa.gov/planetary/'
+export const DEMO_KEY = 'DEMO_KEY'
